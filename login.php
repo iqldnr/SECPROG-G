456f3593317f17ad82335ab4aa65961d7ca00490
IniJamiel <isaiah.jamiel@binus.ac.id>
2022-01-03 20:43:51 +0700

    Update login.php

--- login.php
+++ login.php
@@ -14,62 +14,62 @@
             <a href="index.html"></href><img src="images/logo.png"></a>
             <div class="nav-links-a">
                 <ul>
-                    <li><a href="index.html">HOME</a></li>
-                    <li><a href="produk.html">PRODUK</a></li>
-                    <li><a href="login.html">MASUK</a></li>
-                    <li><a href="tentang.html">TENTANG KAMI</a></li>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="produk.php">PRODUK</a></li>
+                    <li><a href="login.php">MASUK</a></li>
+                    <li><a href="tentang.php">TENTANG KAMI</a></li>
                 </ul>
             </div>
         </nav>
     <div class="text-box-login">
-        <style>   
-            Body {  
-              font-family: Calibri, Helvetica, sans-serif;  
-              background-color: pink;  
-            }  
-            button {   
-                   background-color: #a1a1a1;   
-                   width: 100%;  
-                    color: rgb(0, 0, 0);   
-                    padding: 15px;   
-                    margin: 10px 0px;   
-                    border: none;   
-                    cursor: pointer;   
-                     }   
-             form {   
-                    border: 3px solid #f1f1f1;   
-                }   
-             input[type=text], input[type=password] {   
-                    width: 100%;   
-                    margin: 8px 0;  
-                    padding: 12px 20px;   
-                    display: inline-block;   
-                    border: 2px solid green;   
-                    box-sizing: border-box;   
-                }  
-             button:hover {   
-                    opacity: 0.7;   
-                }   
-                 
-             .container {   
-                    padding: 25px;   
-                    background-color: lightblue;  
-                }   
-            </style>   
-            </head>    
-            <body>    
-                <center> <h1>LOGIN</h1> </center>   
-                <form>  
-                    <div class="container">   
-                        <label>Email : </label>   
-                        <input type="text" placeholder="Masukan Email" name="Email" required>  
-                        <label>Password : </label>   
-                        <input type="password" placeholder="Masukan Password" name="password" required>  
-                        <button type="MASUK">MASUK</button>  
-                    </div>   
-                </form>     
-            </body>     
-            </html>  
+        <style>
+            Body {
+              font-family: Calibri, Helvetica, sans-serif;
+              background-color: pink;
+            }
+            button {
+                   background-color: #a1a1a1;
+                   width: 100%;
+                    color: rgb(0, 0, 0);
+                    padding: 15px;
+                    margin: 10px 0px;
+                    border: none;
+                    cursor: pointer;
+                     }
+             form {
+                    border: 3px solid #f1f1f1;
+                }
+             input[type=text], input[type=password] {
+                    width: 100%;
+                    margin: 8px 0;
+                    padding: 12px 20px;
+                    display: inline-block;
+                    border: 2px solid green;
+                    box-sizing: border-box;
+                }
+             button:hover {
+                    opacity: 0.7;
+                }
+
+             .container {
+                    padding: 25px;
+                    background-color: lightblue;
+                }
+            </style>
+            </head>
+            <body>
+                <center> <h1>LOGIN</h1> </center>
+                <form>
+                    <div class="container">
+                        <label>Email : </label>
+                        <input type="text" placeholder="Masukan Email" name="Email" required>
+                        <label>Password : </label>
+                        <input type="password" placeholder="Masukan Password" name="password" required>
+                        <button type="MASUK">MASUK</button>
+                    </div>
+                </form>
+            </body>
+            </html>
     </div>
 
     </section>
