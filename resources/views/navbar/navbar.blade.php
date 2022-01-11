7a8ce691da244d6c279734c092b64181c8e316c5
IniJamiel <isaiah.jamiel@binus.ac.id>
2022-01-11 14:02:10 +0700

    aa

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta name="viewport" content="with=device-width, initial-scale=1.0">
     <Title>MASUKIN NAMA DISINI</Title>
-    <link rel="stylesheet" href="../css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
@@ -11,26 +10,26 @@
 <body>
     <section class="header">
         <nav>
-            <div class ="object-left fixed">
+            <div>
                 @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    <div class="hidden fixed top-0 left-0 px-6 py-4 sm:block">
                         @auth
-                            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500">DASHBOARD</a>
+                            <a href="{{ url('/dashboard') }}" class="text-gray-700 dark:text-gray-500">DASHBOARD</a>
                         @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500">LOG IN</a>
+                            <a href="{{ route('login') }}" class="text-gray-700 dark:text-gray-500">LOG IN</a>
 
                             @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500">REGISTER</a>
+                                <a href="{{ route('register') }}" class="text-gray-700 dark:text-gray-500">REGISTER</a>
                             @endif
                         @endauth
                     </div>
                 @endif
             </div>
-            <div class="nav-links-a">
+            <div class="nav-links-a hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 <ul>
-                    <li><a href="{{ url('/home') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500">HOME</a></li>
-                    <li><a href="{{ url('/produk') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500">PRODUK</a></li>
-                    <li><a href="{{ url('/tentang') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500">TENTANG KAMI</a></li>
+                    <li><a href="{{ url('/home') }}" class=" text-gray-700 dark:text-gray-500">HOME</a></li>
+                    <li><a href="{{ url('/produk') }}" class="text-gray-700 dark:text-gray-500">PRODUK</a></li>
+                    <li><a href="{{ url('/tentang') }}" class="text-gray-700 dark:text-gray-500">TENTANG KAMI</a></li>
                 </ul>
             </div>
         </nav>
